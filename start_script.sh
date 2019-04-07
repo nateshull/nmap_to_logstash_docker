6a589569074293380c6852b561758aa9f4a3c47f
nateshull <nateshull@gmail.com>
2019-04-07 13:59:21 -0400

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,12 +1,17 @@
 #!/bin/bash 
 echo "running script" 
-if [ "$DEBUG" == "true" ] 
-then 
-	echo "running nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -H "x-nmap-target: $HEADER_INFO" http://$LOGSTASH_SERVER:$LOGSTASH_PORT --data-binary @- " 
-fi 
+
 if [ "$LOGSTASH_USER" = "" ] 
 then 
+	if [ "$DEBUG" == "true" ] 
+	then 
+		echo "running nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -H "x-nmap-target: $HEADER_INFO" $LOGSTASH_SERVER --data-binary @- " 
+	fi 
 	nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -H "x-nmap-target: $SCAN_DESC" $LOGSTASH_SERVER --data-binary @- 
 else 
+	if [ "$DEBUG" == "true" ] 
+	then 
+		echo "running nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl  -u $LOGSTASH_USER:xxxxxxx -H "x-nmap-target: $HEADER_INFO" $LOGSTASH_SERVER --data-binary @- " 
+	fi 
 	nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -u $LOGSTASH_USER:$LOGSTASH_PASS -H "x-nmap-target: $SCAN_DESC" $LOGSTASH_SERVER --data-binary @- 
 fi 
