270c8cc03338c6028d8278d09eb6cf9a10786256
nateshull <nateshull@gmail.com>
2019-04-08 13:44:11 -0400

    Update start_script.sh
    added more debug context.

--- start_script.sh
+++ start_script.sh
@@ -6,12 +6,33 @@ then
 	if [ "$DEBUG" == "true" ] 
 	then 
 		echo "running nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -H "x-nmap-target: $HEADER_INFO" $LOGSTASH_SERVER --data-binary @- " 
+		
+		nmap $NMAP_ARG -oX - > temp.xml
+		echo "************nmap output************"
+		cat temp.xml
+		cat temp.xml | python3 /usr/nmap_logstash/nmap_xml_to_json.py > temp.json
+		echo "************json output************"
+		cat temp.json
+		echo "************upload to logstash************"
+		cat temp.json | curl -H "x-nmap-target: $HEADER_INFO" $LOGSTASH_SERVER --data-binary @-
+	else
+		nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -H "x-nmap-target: $SCAN_DESC" $LOGSTASH_SERVER --data-binary @- 
 	fi 
-	nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -H "x-nmap-target: $SCAN_DESC" $LOGSTASH_SERVER --data-binary @- 
+	
 else 
 	if [ "$DEBUG" == "true" ] 
 	then 
 		echo "running nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl  -u $LOGSTASH_USER:xxxxxxx -H "x-nmap-target: $HEADER_INFO" $LOGSTASH_SERVER --data-binary @- " 
+		nmap $NMAP_ARG -oX - > temp.xml
+		echo "************nmap output************"
+		cat temp.xml
+		cat temp.xml | python3 /usr/nmap_logstash/nmap_xml_to_json.py > temp.json
+		echo "************json output************"
+		cat temp.json
+		echo "************upload to logstash************"
+		cat temp.json | curl -u $LOGSTASH_USER:$LOGSTASH_PASS -H "x-nmap-target: $SCAN_DESC" $LOGSTASH_SERVER --data-binary @-
+	else
+		nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -u $LOGSTASH_USER:$LOGSTASH_PASS -H "x-nmap-target: $SCAN_DESC" $LOGSTASH_SERVER --data-binary @- 
 	fi 
-	nmap $NMAP_ARG -oX - | python3 /usr/nmap_logstash/nmap_xml_to_json.py | curl -u $LOGSTASH_USER:$LOGSTASH_PASS -H "x-nmap-target: $SCAN_DESC" $LOGSTASH_SERVER --data-binary @- 
+	
 fi 
